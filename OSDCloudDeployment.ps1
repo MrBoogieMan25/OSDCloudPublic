2387fbcf358f416df65fbe986f8b32262c1a53a5
MrBoogieMan25 <167644713+MrBoogieMan25@users.noreply.github.com>
2025-01-31 11:21:07 -0500

    Update OSDCloudDeployment.ps1
    Updated Windows versions.

--- OSDCloudDeployment.ps1
+++ OSDCloudDeployment.ps1
@@ -16,10 +16,9 @@ function Show-Menu {
     Write-Host "================ $Title ================"
     Write-Host "1: Press '1' to install Windows 10 Home."
     Write-Host "2: Press '2' to install Windows 11 Home"
-    Write-Host "3: Press '3' to install Windows 11 Enterprise"
+    Write-Host "3: Press '3' to install Windows 11 24H2 Enterprise"
     Write-Host "4: Press '4' for custom installation."
     Write-Host "c: Press 'c' to cancle."
-    Write-Host "NOTICE! Windows 11 24H2 has been pulled by microsoft and will not work!"
 }
 
 Show-Menu -Title 'OS Version Selection'
@@ -29,16 +28,16 @@ $selection = Read-Host "Select your preferred OS installation method"
 switch ($selection) {
     '1' {
         Write-Host "Install started for Windows 10 Home"
-        Start-OSDCloud -OSName 'Windows 10 22h2 x64' -OSLanguage en-us -OSEdition Home -ZTI
+        Start-OSDCloud -OSName 'Windows 10 24h2 x64' -OSLanguage en-us -OSEdition Home -ZTI
 
     }
     '2' {
         Write-Host "Starting Install for Windows 11 Home"
-        Start-OSDCloud -OSName 'Windows 11 22h2 x64' -OSLanguage en-us -OSEdition Home -ZTI
+        Start-OSDCloud -OSName 'Windows 11 24h2 x64' -OSLanguage en-us -OSEdition Home -ZTI
     }
     '3' {
-        Write-Host "Starting Install for Windows 11 23H2 Enterprise"
-        Start-OSDCloud -OSName 'Windows 11 23h2 x64' -OSLanguage en-us -OSEdition Enterprise -ZTI
+        Write-Host "Starting Install for Windows 11 24H2 Enterprise"
+        Start-OSDCloud -OSName 'Windows 11 24h2 x64' -OSLanguage en-us -OSEdition Enterprise -ZTI
     }
     '4' {
         Write-Host "Launching OSDCloudGUI for custom selection"
